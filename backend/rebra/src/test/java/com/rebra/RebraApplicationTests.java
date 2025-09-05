50990333744ffe458b788826c2fc42b6b0605d6f
SwnBae <tngksdk12@naver.com>
2025-09-05 09:58:36 +0900

    init

---
+++ backend/rebra/src/test/java/com/rebra/RebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
