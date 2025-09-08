440e89c077a5c5d0548958fb8aef785ba8059e17
DESKTOP-DLOPF19\SSAFY <ssafy>
2025-09-09 01:29:30 +0900

    fix: LoginPage 디자인 수정

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -16,14 +16,8 @@ export default function SignupPage() {
 
   return (
     <AuthLayout
-      onBack={handleBack}
       title='간편 가입하기'
       subtitle='카카오 계정으로 빠르고 안전하게 시작하세요'
-      buttonText='카카오 계정으로 가입'
-      alternativeText='이미 계정이 있으신가요?'
-      alternativeButtonText='로그인하기'
-      onAlternativeClick={handleLogin}
-      onSubmit={handleKakaoLogin}
     >
       <div className={styles.kakaoButtonContainer}>
         <button className={styles.kakaoButton} onClick={handleKakaoLogin}>
