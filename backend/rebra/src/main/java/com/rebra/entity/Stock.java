91e86e666d27381be4c2967fd4df6689b000167e
SwnBae <tngksdk12@naver.com>
2025-09-10 13:56:28 +0900

    BE - feat: Stock 엔티티 추가

---
+++ backend/rebra/src/main/java/com/rebra/entity/Stock.java
@@ -0,0 +1,44 @@
+package com.rebra.entity;
+
+import com.rebra.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stock")
+public class Stock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "stock_code", nullable = false, unique = true)
+    private String stockCode;
+
+    @Column(name = "stock_name", nullable = false)
+    private String stockName;
+
+    @Column(name = "stock_type", nullable = false)
+    private String stockType;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @Builder
+    public Stock(String stockCode, String stockName, String stockType, Boolean isActive) {
+        this.stockCode = stockCode;
+        this.stockName = stockName;
+        this.stockType = stockType;
+        this.isActive = isActive;
+    }
+}
