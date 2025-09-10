0170487bacbf781817fe09c46236bc8fc912c49d
SwnBae <tngksdk12@naver.com>
2025-09-10 13:33:31 +0900

    BE - refactor: 사용하지 않는 데드 코드 제거

--- backend/rebra/src/main/java/com/rebra/service/SignupService.java
+++ backend/rebra/src/main/java/com/rebra/service/SignupService.java
@@ -1,12 +1,7 @@
 package com.rebra.service;
 
-import com.rebra.dto.request.SignupRequest;
-import com.rebra.dto.response.SignupResponse;
-
 public interface SignupService {
 
     boolean isNicknameAvailable(String nickname);
-    
-    SignupResponse completeSignup(Long userId, SignupRequest signupRequest);
 
 }
