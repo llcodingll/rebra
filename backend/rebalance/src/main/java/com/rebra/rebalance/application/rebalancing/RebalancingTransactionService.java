95b98a7a47e0656313de8e7702b253363c207b4e
SwnBae <SwnBae@users.noreply.github.com>
2025-10-10 10:34:28 +0900

    refactor: RebalancingTransactionService 클래스 레벨 readOnly 트랜잭션 적용

--- backend/rebalance/src/main/java/com/rebra/rebalance/application/rebalancing/RebalancingTransactionService.java
+++ backend/rebalance/src/main/java/com/rebra/rebalance/application/rebalancing/RebalancingTransactionService.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RebalancingTransactionService {
 
     private final RebalancingExecutionRepository executionRepository;
